8631d5457de65af10fb508f0ab5b574631521bb4
albertoanchuste@gmail.com <albertoanchuste@gmail.com>
2023-08-10 18:33:51 +0200

    Buy me a coffee implementation.

--- src/components/BuyACoffee.jsx
+++ src/components/BuyACoffee.jsx
@@ -3,7 +3,7 @@ export default function BuyACoffee () {
         <>
             <h3> Si te ha gustado el trivial y quieres que siga introduciendo nuevas preguntas, mejoras, etc.,</h3>
             <br />
-            <h3 style={{color: "orchid"}}> Por favor, colabora regalándome un cafecito, pinchando sobre el link de abajo. Es muy fácil.</h3>
+            <h3 style={{color: "orchid"}}> por favor, colabora regalándome un cafecito, pinchando sobre el link de abajo. Es muy fácil.</h3>
             <br />
             <a href='https://ko-fi.com/D1D0O2IDM' target='_blank'>
                 <img  style={{border: "0px", height:"3.5em", marginBottom:"1.5em"}} 
