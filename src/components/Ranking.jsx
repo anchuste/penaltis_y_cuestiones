9c1ab57c1762d888ebf907dc48d8c326d0f58c72
albertoanchuste@gmail.com <albertoanchuste@gmail.com>
2023-09-13 22:46:24 +0200

    Added spinner to main ranking screen

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -2,18 +2,31 @@ import HeaderStockInfoTable from './HeaderStockInfoTable';
 import RowsStockInfoTable from './RowsStockInfoTable';
 import { useEffect, useState, useRef } from 'react';
 import {getTopPoints} from './../services/points-service.js'
+import PuffLoader from "react-spinners/PuffLoader";
 
 export const Ranking = ({points, title}) => {
 
     const [pointsRecovered, setpointsRecovered] = useState([]);
+    const [showLoading, setshowLoading] = useState(false);
+    //const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const CSSProperties = {
+      display: "block",
+      margin: "0 auto",
+      marginTop: "1em",
+      borderColor: "red",
+    };
 
     useEffect(() => {
 
         //console.log("Se ejecuta el useEffect!!!")
 
+        setshowLoading(true);
         async function fetchData() {
           const response = await getTopPoints(points);
+          //await delay(5000);
           setpointsRecovered(response);
+          setshowLoading(false)
         }
         fetchData();
     
@@ -27,6 +40,15 @@ export const Ranking = ({points, title}) => {
 	            <HeaderStockInfoTable></HeaderStockInfoTable>
                 <RowsStockInfoTable stocksInfo={pointsRecovered}></RowsStockInfoTable>
             </table>
+            <PuffLoader
+                color={"papayawhip"}
+                loading={showLoading}
+                size={35}
+                cssOverride={CSSProperties}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+                title='Hola'
+            />
         </div>
     );
 }
