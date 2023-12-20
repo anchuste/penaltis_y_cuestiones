ad9fb11d61938fd7dbd1a64993dc42faee9d0848
albertoanchuste@gmail.com <albertoanchuste@gmail.com>
2023-12-20 21:19:51 +0100

    Refactor TestQuestion component to use useState and useEffect hooks

--- src/components/TestQuestion.jsx
+++ src/components/TestQuestion.jsx
@@ -2,17 +2,41 @@ import { Square } from './Square.jsx'
 import Countdown from 'react-countdown';
 import * as constants from '../constants/index.js';
 import data from '../assets/questions/questions.json';
+import { useState, useEffect } from 'react';
+import { getQuestions } from '../services/question-service.js';
 
 export const TestQuestion = ({ questionIndex }) => {
 
     console.log('Se renderiza el componente TestQuestion');
 
-    let questions = data;
-    const QUESTIONS_NUMBER = questions.length;
+    const [questionToTest, setQuestionToTest] = useState();
 
-    let questionToTest = questions[questionIndex];
+    if (questionToTest != undefined) {
+      questionToTest.answersFormatted = questionToTest.answers.split('**');
+    }
+
+    let questions;
+    
+
+    //let questionToTest = questions[questionIndex];
 
     let currentSeconds = 0;
+
+    useEffect(() => {
+
+      console.log('Se renderiza el componente TestQuestion useEffect');
+
+      async function fetchQuestionData() {
+        const response = await getQuestions();
+        questions = response;
+        const indexCuestion = questions.findIndex(item => item.id_question === questionIndex);
+        let questionToTestAux = questions[indexCuestion];
+        setQuestionToTest(questionToTestAux);
+      }
+  
+      fetchQuestionData();
+      
+    }, []);
 
     // Renderer callback with condition
     const renderer = ({ seconds, completed, api, props }) => {
@@ -28,7 +52,7 @@ export const TestQuestion = ({ questionIndex }) => {
     return (
       <div>
 
-        {questionToTest.image ? 
+        {questionToTest != undefined && questionToTest.image ? 
           <img src={questionToTest.image} className='question_image' alt='React Logo' /> : null}
 
           <div>
@@ -36,21 +60,23 @@ export const TestQuestion = ({ questionIndex }) => {
           </div>
           <div>
           <h2 className='time_to_go_question_time'> ⌚ </h2>
-          
+
+          {questionToTest !=undefined &&
           <Countdown
               date={Date.now() + constants.TIME_TO_GO_QUESTIONS}
               renderer={renderer}
               onComplete={(event, api) => console.log('event api', api)}
-              key={questionToTest.id}
-          />
-
+              key={questionToTest.id_question}
+          />}
+          
           </div>
 
-          <h2 className='customDiv'>{questionToTest.text}</h2>
+          {questionToTest != undefined ?
+          <h2 className='customDiv'>{questionToTest.question}</h2>: null}
           <section>
           <ol>
           {
-            questionToTest.answers.map((answer, index) => {
+            questionToTest != undefined && questionToTest.answersFormatted.map((answer, index) => {
               return (
                 <li className='answers' key={index} onClick={() => updateQuestion(questionToTest.id, index, currentSeconds)}
                 >
@@ -60,7 +86,8 @@ export const TestQuestion = ({ questionIndex }) => {
             })
           }
           </ol>
-          <h5 style={{marginTop: "8px"}}>Autor de la pregunta: {questionToTest.author}</h5>
+          {questionToTest != undefined ?
+          <h5 style={{marginTop: "8px"}}>Autor de la pregunta: {questionToTest.author}</h5>:null}
         </section>
       </div>
     )
